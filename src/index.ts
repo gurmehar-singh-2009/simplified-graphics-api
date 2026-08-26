7a5f22bd3204d843034d2dc3e05403a24c6bc6a6
Umar Ahmed <umarahmed02009@gmail.com>
2026-08-26 16:37:26 -0400

    Use perspective camera of quick test.

--- src/index.ts
+++ src/index.ts
@@ -37,12 +37,11 @@ engine.resize(window.innerWidth, window.innerHeight);
 
 engine.start();
 
-let cam = new OrthographicCamera(1920, 16 / 9, -1, 1, new Vector3(0, 0, 0));
+let cam = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000, new Vector3(-100, 0, -100), Quaternion.fromAxisAngle(new Vector3(0, 1, 0), Math.PI/4));
 
 let prev = 0;
 
 engine.onFrame = (renderer, timestamp) => {
-    console.log(1000 / (timestamp - prev))
     renderer.setCamera(cam);
 
     renderer.clear(200, 200, 200, 1);
